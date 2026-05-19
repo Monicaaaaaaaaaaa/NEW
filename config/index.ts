4df7d0ea054a768c5e26222538d17d153bcd3908
Monica Oguntona <Monica.Oguntona@etranzact.com>
2026-05-19 12:11:59 +0100

    Updated login UI

--- config/index.ts
+++ config/index.ts
@@ -8,26 +8,8 @@ const config = {
     promptBeforeIdleTimeout: 1000 * 60 * 3, // 3 minutes
     protectedRoutes: ["/dashboard", "/profile", "/settings"],
     publicRoutes: ["/", "/forgot-password"],
-    manualEntryLineItemLimit: 5,
-    maximumFileUploadSize: 1024 * 1000 * 5, // 5MB
-    auth: {
-        cookieName: "merchant-session",
-        tokenRefreshThreshold: 2 * 60 * 1000, // Refresh token 2 minutes before expiry
-        sessionTTL: 7 * 24 * 60 * 60 * 1000, // 7 days
-        // isSecure: process.env.IS_COOKIE_SECURE === "true",
-        // authPassword: process.env.AUTH_SECRET,
-    },
-    passwordMinLength: 12,
-    defaultPageSize: {
-        default: 20,
-        invoiceTable: 1_000,
-        historyTable: 10,
-    },
 };
- 
-// NOTE: difference between idleTimeout and promptBeforeIdleTimeout is when the idle timer modal will pop up
-// Idletimeout - promptBeforeIdleTimeout
- 
+
 export default config;
  
  
